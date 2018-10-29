50a763ab93adc0a191505ab21737e73f720b3ab7
elised2 <33736944+elised2@users.noreply.github.com>
2018-10-29 18:41:26 -0400

    Create LogIn.vb
    create the LogIn.vb class in accordance to the seat class from the in class work

---
+++ LogIn.vb
@@ -0,0 +1,12 @@
+'The LogIn class works closely with the Welcome form to allow the uesr to log in
+Public Class LogIn
+    'EDD several Public Property variables holding the attributes from the dataset
+    Public Property username As String
+    Public Property passwrod As String
+
+    'EDD: creates a list of LogIn objects for the user
+    Public Shared Property LogInList As New List(Of LogIn)
+    Public Sub New()
+        'EDD: constrctor for the LogIn class
+    End Sub
+End Class
