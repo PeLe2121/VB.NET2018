b5fb866b6d03ddde42f328a49d2afdf8efb6ab20
PeLe2121 <44589273+PeLe2121@users.noreply.github.com>
2018-10-30 01:30:58 -0400

    Update Profit_Contribution.vb
    error in data type

--- Profit_Contribution.vb
+++ Profit_Contribution.vb
@@ -2,7 +2,7 @@ Public Class Profit_Contribution
 
     'PC: several Public Property variables holding the attributes from the dataset
     Public Property name As String
-    Public Property value As Short
+    Public Property value As Single
     Public Property unitOfMeasurement As String
 
     'PC: creates a list of Profit_Contribution objects for the user
