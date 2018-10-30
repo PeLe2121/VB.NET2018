845c6beb22cf3b464adc3bcfcc1b7786d23f7f41
PeLe2121 <44589273+PeLe2121@users.noreply.github.com>
2018-10-30 01:10:41 -0400

    Create Profit_Contribution.vb
    Created the Profit_Contribution class

---
+++ Profit_Contribution.vb
@@ -0,0 +1,14 @@
+Public Class Profit_Contribution
+
+    'PC: several Public Property variables holding the attributes from the dataset
+    Public Property name As String
+    Public Property value As Short
+    Public Property unitOfMeasurement As String
+
+    'PC: creates a list of Profit_Contribution objects for the user
+    'PC: ProfitCont is short for "discount condiditon"
+    Public Shared Property ProfitContList As New List(Of Profit_Contribution)
+    Public Sub New()
+            'PC: constrctor for the Profit_Contribution class
+        End Sub
+End Class
